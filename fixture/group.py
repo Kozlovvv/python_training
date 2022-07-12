402d9e94e8aeaefcecc0c2c0371d0e24ba71787d
Vladimir Kozlov <kozlovtp@yandex.ru>
2022-07-12 19:50:43 +0300

    Добавлен кейс редактирования группы с очисткой и заполнением полей.

--- fixture/group.py
+++ fixture/group.py
@@ -1,5 +1,3 @@
-
-
 class GroupHelper:
 
     def __init__(self, app):
@@ -40,3 +38,23 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_g_page()
+
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_g_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit edit
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit update
+        wd.find_element_by_name("update").click()
