31de8a1a9332a06471aab5f700f4726996f905a0
Vladimir Kozlov <kozlovtp@yandex.ru>
2022-07-13 12:22:51 +0300

    1) Обратите внимание на то, что у Вас в методах удаления и модификации контакта отсутствует в начале переход на страницу контактов. Если мы изначально не находимся на этой странице после выполнения предыдущего сценария, то сценарии модификации и удаления контакта будут падать.
    2) Хотелось бы видеть переиспользование кода. Почему метод создания нового контакта практически идентичен методу модификации контакта и там большая часть кода дублируется?
    
    Нам не нужны две отдельных фикстуры для контактов и для групп (ну и два разных класса-менеджера соответственно тоже не нужны). Нужно просто добавить в Application метод для создания контактов и все.

--- fixture/contact.py
+++ fixture/contact.py
@@ -3,11 +3,7 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
-    def create_contact(self, contact):
-        wd = self.app.wd
-        # init contact creation
-        wd.find_element_by_link_text("add new").click()
-        # fill contacts form
+    def fill_contact_form(self, wd, contact):
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").clear()
@@ -16,6 +12,12 @@ class ContactHelper:
         wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("mobile").clear()
         wd.find_element_by_name("mobile").send_keys(contact.mobile)
+
+    def create_contact(self, contact):
+        wd = self.app.wd
+        # init contact creation
+        wd.find_element_by_link_text("add new").click()
+        self.fill_contact_form(wd, contact)
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
@@ -35,14 +37,6 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("home").click()
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
-        # fill group form
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.midlename)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        self.fill_contact_form(wd, contact)
         # submit update
         wd.find_element_by_name("update").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.create_contact(Contact(firstname= "Vladimir", midlename= "Vladimirovich", lastname= "Kozlov", mobile= "1234567890"))
+    app.contact.create_contact(Contact(firstname="Vladimir", midlename="Vladimirovich", lastname="Kozlov", mobile="1234567890"))
     app.session.logout()
 
 
@@ -11,4 +11,3 @@ def test_add_emty_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(firstname="", midlename="", lastname="", mobile=""))
     app.session.logout()
-
