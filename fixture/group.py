70a112b1f03559ef19b3ecc74d306fe5bd43b5e6
Vladimir Kozlov <kozlovtp@yandex.ru>
2022-07-13 12:32:50 +0300

    1) Обратите внимание на то, что у Вас в методах удаления и модификации контакта отсутствует в начале переход на страницу контактов. Если мы изначально не находимся на этой странице после выполнения предыдущего сценария, то сценарии модификации и удаления контакта будут падать.
    2) Хотелось бы видеть переиспользование кода. Почему метод создания нового контакта практически идентичен методу модификации контакта и там большая часть кода дублируется?
    
    Нам не нужны две отдельных фикстуры для контактов и для групп (ну и два разных класса-менеджера соответственно тоже не нужны). Нужно просто добавить в Application метод для создания контактов и все.
    
      Так же добавленна общая фикстура для всех тестов.

--- fixture/group.py
+++ fixture/group.py
@@ -12,16 +12,7 @@ class GroupHelper:
         self.open_g_page()
         # init group creation
         wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(wd, group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_g_page()
@@ -46,7 +37,11 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         # submit edit
         wd.find_element_by_name("edit").click()
-        # fill group form
+        self.fill_group_form(wd, group)
+        # submit update
+        wd.find_element_by_name("update").click()
+
+    def fill_group_form(self, wd, group):
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -56,5 +51,3 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit update
-        wd.find_element_by_name("update").click()
