22d28e693613b148091695d8798dee7bef698d88
Vladimir Kozlov <kozlovtp@yandex.ru>
2022-07-14 14:51:27 +0300

    Не рабочий код.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,9 +2,12 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(firstname="Vladimir", midlename="Vladimirovich", lastname="Kozlov", mobile="1234567890"))
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
+    app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(firstname="", midlename="", lastname="", mobile=""))
-
+    app.session.logout()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,16 +2,24 @@ from model.contact import Contact
 
 
 def test_edit_contact_firstname(app):
+    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(firstname="New firstname"))
+    app.session.logout()
 
 
 def test_edit_contact_midlename(app):
+    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(midlename="New midlename"))
+    app.session.logout()
 
 
 def test_edit_contact_lastname(app):
+    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(lastname="New lastname"))
+    app.session.logout()
 
 
 def test_edit_contact_mobile(app):
+    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(mobile="88005354242"))
+    app.session.logout()
